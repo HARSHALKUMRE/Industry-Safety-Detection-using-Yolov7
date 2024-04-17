25dcaf33b7bd77c44e419407cd5fbf63f2c5bfc7
HARSHALKUMRE <kumreharshalkumar@gmail.com>
2024-04-17 14:32:23 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'industry-safety',
+    version= '0.0.0',
+    author= 'Harshal Kumre',
+    author_email= 'kumreharshalkumar@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
