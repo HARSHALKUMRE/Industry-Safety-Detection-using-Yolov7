869a87b44a02c3d7bf97d6e60a72ede4d5d0d867
HARSHALKUMRE <kumreharshalkumar@gmail.com>
2024-04-19 14:19:09 +0530

    model trainer added

--- industry/pipeline/training_pipeline.py
+++ industry/pipeline/training_pipeline.py
@@ -4,18 +4,22 @@ from industry.exception import IndustryException
 from industry.configuration.s3_operations import S3Operation
 from industry.components.data_ingestion import DataIngestion
 from industry.components.data_validation import DataValidation
+from industry.components.model_trainer import ModelTrainer
 
 from industry.entity.config_entity import (DataIngestionConfig,
-                                           DataValidationConfig)
+                                           DataValidationConfig,
+                                           ModelTrainerConfig)
 
 from industry.entity.artifacts_entity import (DataIngestionArtifact,
-                                              DataValidationArtifact)
+                                              DataValidationArtifact,
+                                              ModelTrainerArtifact)
 
 
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
         self.data_validation_config = DataValidationConfig()
+        self.model_trainer_config = ModelTrainerConfig()
         
         
     def start_data_ingestion(self)-> DataIngestionArtifact:
@@ -65,6 +69,19 @@ class TrainPipeline:
             raise IndustryException(e, sys) from e
         
         
+    def start_model_trainer(self
+    ) -> ModelTrainerArtifact:
+        try:
+            model_trainer = ModelTrainer(
+                model_trainer_config=self.model_trainer_config,
+            )
+            model_trainer_artifact = model_trainer.initiate_model_trainer()
+            return model_trainer_artifact
+
+        except Exception as e:
+            raise IndustryException(e, sys) from e
+        
+        
     def run_pipeline(self) -> None:
         try:
             data_ingestion_artifact = self.start_data_ingestion()
@@ -72,6 +89,13 @@ class TrainPipeline:
             data_validation_artifact = self.start_data_validation(
                 data_ingestion_artifact=data_ingestion_artifact
             )
+            
+            if data_validation_artifact.validation_status == True:
+                model_trainer_artifact = self.start_model_trainer()
+            
+            else:
+                raise Exception("Your data is not in correct format")
+
 
         except Exception as e:
-            raise IndustryException(e, sys)
+            raise IndustryException(e, sys) from e
