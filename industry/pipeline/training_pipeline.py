4afc172bb9f9b9cc5c8de5662fa24aa757ee458f
HARSHALKUMRE <kumreharshalkumar@gmail.com>
2024-04-19 17:23:33 +0530

    model pusher added

--- industry/pipeline/training_pipeline.py
+++ industry/pipeline/training_pipeline.py
@@ -5,14 +5,17 @@ from industry.configuration.s3_operations import S3Operation
 from industry.components.data_ingestion import DataIngestion
 from industry.components.data_validation import DataValidation
 from industry.components.model_trainer import ModelTrainer
+from industry.components.model_pusher import ModelPusher
 
 from industry.entity.config_entity import (DataIngestionConfig,
                                            DataValidationConfig,
-                                           ModelTrainerConfig)
+                                           ModelTrainerConfig,
+                                           ModelPusherConfig)
 
 from industry.entity.artifacts_entity import (DataIngestionArtifact,
                                               DataValidationArtifact,
-                                              ModelTrainerArtifact)
+                                              ModelTrainerArtifact,
+                                              ModelPusherArtifact)
 
 
 class TrainPipeline:
@@ -20,6 +23,8 @@ class TrainPipeline:
         self.data_ingestion_config = DataIngestionConfig()
         self.data_validation_config = DataValidationConfig()
         self.model_trainer_config = ModelTrainerConfig()
+        self.model_pusher_config = ModelPusherConfig()
+        self.s3 = S3Operation()
         
         
     def start_data_ingestion(self)-> DataIngestionArtifact:
@@ -81,6 +86,21 @@ class TrainPipeline:
         except Exception as e:
             raise IndustryException(e, sys) from e
         
+    def start_model_pusher(self, model_trainer_artifact: ModelTrainerArtifact, s3: S3Operation):
+
+        try:
+            model_pusher = ModelPusher(
+                model_pusher_config=self.model_pusher_config,
+                model_trainer_artifact= model_trainer_artifact,
+                s3=s3
+                
+            )
+            model_pusher_artifact = model_pusher.initiate_model_pusher()
+            return model_pusher_artifact
+        except Exception as e:
+            raise IndustryException(e, sys)
+    
+        
         
     def run_pipeline(self) -> None:
         try:
@@ -92,6 +112,7 @@ class TrainPipeline:
             
             if data_validation_artifact.validation_status == True:
                 model_trainer_artifact = self.start_model_trainer()
+                model_pusher_artifact = self.start_model_pusher(model_trainer_artifact=model_trainer_artifact,s3=self.s3_operations)
             
             else:
                 raise Exception("Your data is not in correct format")
